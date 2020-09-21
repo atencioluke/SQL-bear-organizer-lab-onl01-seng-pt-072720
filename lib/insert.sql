05102e3ae47310edd025f6cbd15242d8ca0ae96d
atencioluke <atencioluke@gmail.com>
2020-09-21 09:42:25 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+('Mr. Chocolate', 90, 'male', 'brown', 'chill', false),
+('Rowdy', 22, 'male', 'brown', 'angry', true),
+('Tabitha', 45, 'female', 'brown', 'angry', false),
+('Sergeant Brown', 8, 'female', 'brown', 'angry', true),
+('Melissa', 1, 'female', 'brown', 'angry', true),
+('Grinch', 5, 'male', 'brown', 'angry', true),
+('Wendy', 4, 'male', 'brown', 'angry', true),
+(null, 2, '', 'brown', 'angry', true);
